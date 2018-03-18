100c312e3dd608df88e1c3138eac9b888f42a8c2
Fan <fancheung@outlook.com>
2018-03-19 07:59:30 +0800

    add test key

--- push.js
+++ push.js
@@ -1,24 +1,47 @@
 const webPush = require('web-push')
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
 
-webPush.setGCMAPIKey(process.env.GCM_API_KEY || null)
+const vapidKeys = {
+  publicKey:
+'BI5oyws4LvhV0Na6GWLQp_RRW6Sw9sjoIYUYdS26OMJOYdHevmVPH59HhkcXxUP5PkBA1lrUz5_SN4nPa4MazY4',
+  privateKey: 'CyE04R9_c3xwo_36kdtQ06TvTR3PYvEj1qKCgFiZRSs'
+};
 
-app.post('/register', function (req, res) {
-  res.sendStatus(201)
-})
+webPush.setVapidDetails(
+  'mailto:web-push-book@gauntface.com',
+  vapidKeys.publicKey,
+  vapidKeys.privateKey
+);
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE')
+  if (req.method === 'OPTIONS') {
+    return res.send(200)
+  }
+  next();
+});
 
-app.post('/sendNotification', function (req, res) {
+// app.post('/save-subscription', function (req, res) {
+//   console.log(req.body)
+//   res.send(200)
+// })
+
+app.post('/save-subscription', function (req, res) {
 
   setTimeout(function () {
+    console.log('ahdsfasf')
     webPush.sendNotification({
       endpoint: req.body.endpoint,
-      TTL: req.body.ttl,
       keys: {
         p256dh: req.body.key,
         auth: req.body.authSecret
       }
-    }, req.body.payload)
+    })
       .then(function () {
         res.sendStatus(201);
       })
@@ -26,7 +49,7 @@ app.post('/sendNotification', function (req, res) {
         console.log(error);
         res.sendStatus(500);
       });
-  }, req.body.delay * 1000);
+  },  4000);
 })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
