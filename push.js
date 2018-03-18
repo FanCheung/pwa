931b6fd90656959ab7e6fd5be6368f2fbb56c209
Fan <fancheung@outlook.com>
2018-03-18 22:32:56 +0800

    make server

--- push.js
+++ push.js
@@ -1,28 +1,33 @@
-var webPush = require('web-push');
+const webPush = require('web-push')
+const express = require('express')
+const app = express()
 
-webPush.setGCMAPIKey(process.env.GCM_API_KEY || null);
-module.exports = function(app, route) {
-  app.post(route + 'register', function(req, res) {
-    res.sendStatus(201);
-  });
+webPush.setGCMAPIKey(process.env.GCM_API_KEY || null)
 
-  app.post(route + 'sendNotification', function(req, res) {
-    setTimeout(function() {
-      webPush.sendNotification({
-        endpoint: req.body.endpoint,
-        TTL: req.body.ttl,
-        keys: {
-          p256dh: req.body.key,
-          auth: req.body.authSecret
-        }
-      }, req.body.payload)
-      .then(function() {
+app.post('/register', function (req, res) {
+  res.sendStatus(201)
+})
+
+app.post('/sendNotification', function (req, res) {
+
+  setTimeout(function () {
+    webPush.sendNotification({
+      endpoint: req.body.endpoint,
+      TTL: req.body.ttl,
+      keys: {
+        p256dh: req.body.key,
+        auth: req.body.authSecret
+      }
+    }, req.body.payload)
+      .then(function () {
         res.sendStatus(201);
       })
-      .catch(function(error) {
+      .catch(function (error) {
         console.log(error);
         res.sendStatus(500);
       });
-    }, req.body.delay * 1000);
-  });
-};
+  }, req.body.delay * 1000);
+})
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
